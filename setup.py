4540598974f993d5cd893f631c32b33a4eab1fc8
Robert Niederreiter <rnix@squarewave.at>
2011-01-20 20:29:37 +0100

    sources from github

--- setup.py
+++ setup.py
@@ -4,16 +4,15 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '3-dev'
+version = 'dev'
 shortdesc = 'AGX development Bundle.'
-longdesc = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read()
+longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
-setup(name='agx.flavour.dev',
+setup(name='agx.dev',
       version=version,
       description=shortdesc,
       long_description=longdesc,
       classifiers=[
-            'Development Status :: 3 - Alpha',
             'License :: OSI Approved :: GNU General Public License (GPL)',
             'Operating System :: OS Independent',
             'Programming Language :: Python', 
@@ -21,11 +20,11 @@ setup(name='agx.flavour.dev',
       keywords='AGX, Code Generator',
       author='BlueDynamics Alliance',
       author_email='dev@bluedynamics.com',
-      url=u'https://svn.plone.org/svn/archetypes/AGX',
+      url=u'https://github.com/bluedynamics/agx.dev',
       license='GNU General Public Licence',
       packages=find_packages('src'),
       package_dir = {'': 'src'},
-      namespace_packages=['agx', 'agx.flavour'],
+      namespace_packages=['agx'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
@@ -35,12 +34,12 @@ setup(name='agx.flavour.dev',
       ],
       extras_require = dict(
           test=[
-            'interlude',
+              'interlude',
           ]
       ),
       entry_points={
-        'console_scripts': [
-            'agx = agx.flavour.dev.main:run',
-            ],
+          'console_scripts': [
+              'agx = agx.flavour.dev.main:run',
+          ],
       },
       )
