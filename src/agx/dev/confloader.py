d3845041935e923357e1c4154603cdaeba462a90
Robert Niederreiter <rnix@squarewave.at>
2012-01-31 17:29:26 +0100

    load agx.generator.gs

--- src/agx/dev/confloader.py
+++ src/agx/dev/confloader.py
@@ -8,6 +8,7 @@ from agx.core.interfaces import IConfLoader
 import agx.generator.uml
 import agx.generator.pyegg
 import agx.generator.zca
+import agx.generator.gs
 
 class ConfLoader(object):
     
@@ -45,3 +46,4 @@ class ConfLoader(object):
         XMLConfig('configure.zcml', agx.generator.uml)()
         XMLConfig('configure.zcml', agx.generator.pyegg)()
         XMLConfig('configure.zcml', agx.generator.zca)()
+        XMLConfig('configure.zcml', agx.generator.gs)()
