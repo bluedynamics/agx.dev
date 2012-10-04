ffddd83b63fccb166b6818702ba1ff8bdc2608d1
Robert Niederreiter <office@squarewave.at>
2012-10-04 11:07:48 +0200

    no coverage for now

--- ci.sh
+++ ci.sh
@@ -4,24 +4,23 @@ virtualenv env
 python bootstrap.py -c dev.cfg
 bin/buildout -c dev.cfg
 ls bin/
-bin/coverage odict
-bin/coverage plumber
-bin/coverage node.ext.xml
-bin/coverage node.ext.xmi
-bin/coverage node.ext.zcml
-bin/coverage node.ext.uml
-bin/coverage node.ext.python
-bin/coverage node.ext.template
-bin/coverage node.ext.directory
-bin/coverage agx.core
-bin/coverage agx.transform.xmi2uml
-bin/coverage agx.transform.uml2fs
-bin/coverage agx.generator.zca
-bin/coverage agx.generator.uml
-bin/coverage agx.generator.sql
-bin/coverage agx.generator.pyegg
-bin/coverage agx.generator.plone
-bin/coverage agx.generator.generator
-bin/coverage agx.generator.dexterity
-bin/coverage agx.generator.buildout
-bin/coveragereport
+bin/test odict
+bin/test plumber
+bin/test node.ext.xml
+bin/test node.ext.xmi
+bin/test node.ext.zcml
+bin/test node.ext.uml
+bin/test node.ext.python
+bin/test node.ext.template
+bin/test node.ext.directory
+bin/test agx.core
+bin/test agx.transform.xmi2uml
+bin/test agx.transform.uml2fs
+bin/test agx.generator.zca
+bin/test agx.generator.uml
+bin/test agx.generator.sql
+bin/test agx.generator.pyegg
+bin/test agx.generator.plone
+bin/test agx.generator.generator
+bin/test agx.generator.dexterity
+bin/test agx.generator.buildout
