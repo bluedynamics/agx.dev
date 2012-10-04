c8f173a18039bf94a3be0cbde548e2c88006e7ab
Robert Niederreiter <office@squarewave.at>
2012-10-04 13:43:11 +0200

    split up ci.sh

--- ci.sh
+++ ci.sh
@@ -1,26 +1,5 @@
-python --version
 python2.6 --version
 virtualenv env
 python bootstrap.py -c dev.cfg
 bin/buildout -c dev.cfg
-ls bin/
-bin/test odict
-bin/test plumber
-bin/test node.ext.xml
-bin/test node.ext.xmi
-bin/test node.ext.zcml
-bin/test node.ext.uml
-bin/test node.ext.python
-bin/test node.ext.template
-bin/test node.ext.directory
-bin/test agx.core
-bin/test agx.transform.xmi2uml
-bin/test agx.transform.uml2fs
-bin/test agx.generator.zca
-bin/test agx.generator.uml
-bin/test agx.generator.sql
-bin/test agx.generator.pyegg
-bin/test agx.generator.plone
-bin/test agx.generator.generator
-bin/test agx.generator.dexterity
-bin/test agx.generator.buildout
+./test.sh
